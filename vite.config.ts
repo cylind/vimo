764fcc689935792b049d5eb4a364430ef323ed2d
chan <cylind@proton.me>
2025-05-15 17:04:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,16 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: resolve(__dirname, 'src/index.html'),
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (name === 'index.html') {
+            return '[name].[ext]'; // 输出 index.html 到 dist/index.html
+          }
+          return 'assets/[name]-[hash].[ext]';
+        },
+      },
     },
   },
 });
