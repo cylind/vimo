0172cb8b8bff2797fdaab086f33c8e4c309d9e1f
chan <cylind@proton.me>
2025-05-15 11:56:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import devServer from '@hono/vite-dev-server';
-import adapter from '@hono/vite-dev-server/cloudflare';
-import build from '@hono/vite-cloudflare-pages';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    devServer({
-      entry: 'src/index.tsx',
-      adapter,
-    }),
-    build(),
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
